2d5346612dc1b43fb60445536fb958b0d733392c
Fiona McDonald <fiona.mcdonald@digital.hmrc.gov.uk>
2016-02-20 16:41:02 +0000

    email or phone optional

--- app/models/guest_detail.rb
+++ app/models/guest_detail.rb
@@ -2,5 +2,12 @@ class GuestDetail < ActiveRecord::Base
 
   validates :attending, presence: true
   validates :guest_1_name, presence: true
+  validates :children, numericality: { only_integer: true, greater_than: 0 }
+
+  validate :validate_email_or_phone_present
+
+  def validate_email_or_phone_present
+    errors.add(:base, "Either phone or email is required") unless phone.present? || email.present?
+  end
 
 end
