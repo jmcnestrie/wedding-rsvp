b863d4041f4000264aae4b68cf53afbdedf41085
Steve Hobbs <elkdanger@gmail.com>
2016-03-05 23:34:50 +0000

    Added index action + test to AdminController

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   get '/' => 'home#index'
+  get '/admin' => 'admin#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
