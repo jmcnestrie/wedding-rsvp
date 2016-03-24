3d54187bbe9c97d0707aec1b262369d80af7845b
Steve Hobbs <elkdanger@gmail.com>
2016-03-24 21:56:00 +0000

    Basic mailer

--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -1,2 +1,10 @@
 class RsvpMailer < ApplicationMailer
+
+  def notification
+
+    recipients = ['elkdanger@gmail.com', 'fiona.m.mcdonald@gmail.com']
+
+    mail to: recipients, subject: 'Someone has sent an RSVP!'
+  end
+
 end
--- spec/mailers/rsvp_mailer_spec.rb
+++ spec/mailers/rsvp_mailer_spec.rb
@@ -4,8 +4,18 @@ RSpec.describe RsvpMailer, type: :mailer do
 
   describe "the notification mailer" do
 
-    it "renders the notification template" do
-    
+    let (:mail) { RsvpMailer.notification }
+
+    it 'sets the correct subject' do
+      expect(mail.subject).to eql 'Someone has sent an RSVP!'
+    end
+
+    it 'sends it to Steve' do
+      expect(mail.to).to include 'elkdanger@gmail.com'
+    end
+
+    it 'sends it to Fiona' do
+      expect(mail.to).to include 'fiona.m.mcdonald@gmail.com'
     end
 
   end
